a72b11a713ab8bf993e510a6f39126cef888db7c
jethrodaniel <jethrodaniel@gmail.com>
2018-09-17 10:48:30 -0500

    Add dark background, white text

--- App.js
+++ App.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View } from 'react-native';
+
+const Header = (props) => (
+  <Text style={styles.header}>{props.message}</Text>
+);
+
+class InputPage extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header message="Enter Scheduled Time" />
+        <Text style={styles.header}>Yo!</Text>
+      </View>
+    );
+  }
+}
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.app}>
+        <InputPage />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#282923',
+  },
+  container: {
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    marginTop: 64,
+    color: 'white',
+    fontSize: 32,
+    fontWeight: 'bold'
+  }
 });
