e1d46c45425f6be6c3c39c3f82310c18f66b52eb
Mark Delk <jethrodaniel@gmail.com>
2018-12-16 09:32:26 -0600

    start separating components

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Button } from 'react';
+
 import {
   StyleSheet,
   Text,
@@ -6,70 +7,40 @@ import {
   View
 } from 'react-native';
 
-const Header = (props) => (
-  <Text style={styles.header}>{props.message}</Text>
-);
+import HourInput from './components/HourInput';
 
-export class HourInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: '' };
-  }
+// const Header = (props) => (
+//   <Text style={styles.header}>{props.message}</Text>
+// );
 
+export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <TextInput
-          style={{width: 160, height: 62, borderColor: 'gray', borderWidth: 1, color: 'white', fontSize: 26}}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-          placeholder="Hours"
-          keyboardType="numeric"
-          returnKeyType="next"
-        />
-      </View>
-    );
-  }
-}
-
-class InputPage extends React.Component {
-  render() {
-    return (
-      <View style={{
-        alignItems: 'center',
-        flexDirection: 'column',
-        justifyContent: 'space-around',
-        flex: 1
-      }}>
+      <View style={styles.app}>
 
         <View>
           <Text style={{color: 'white', fontSize: 32}}>
             Enter scheduled time
           </Text>
         </View>
-          <HourInput/>
-          <HourInput/>
-          <HourInput/>
-      </View>
-    );
-  }
-}
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.app}>
-        <InputPage/>
-      </View>
+        <HourInput/>
+        <HourInput/>
+        <HourInput/>
+
+        </View >
     );
   }
 }
 
 const styles = StyleSheet.create({
   app: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
     flex: 1,
     backgroundColor: '#282923',
-    paddingTop: 10,
+    paddingTop: 10
   },
   header: {
     marginTop: 64,
