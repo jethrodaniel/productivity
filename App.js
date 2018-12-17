7d73d18878a15e4b531b8845d4ff084f819c243f
Mark Delk <jethrodaniel@gmail.com>
2018-12-16 20:40:39 -0600

    make input fields necessary to get to results page

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { StyleSheet } from 'react-native';
+import { Alert, StyleSheet } from 'react-native';
 
 import Header from './components/Header';
 import NumberInput from './components/NumberInput';
@@ -21,13 +21,23 @@ export default class App extends React.Component {
 
   // Logs a button press to the console
   logPress = () => {
-    console.log(`Pressed a button! submitted: ${this.state.submitted}`);
+    console.log(`
+      Pressed a button! submitted: ${this.state.submitted}
+
+      hours: ${this.state.hours}
+      minutes: ${this.state.minutes}
+      rate: ${this.state.rate}
+    `);
   }
 
   // Submits the input page
   submit = () => {
     this.logPress();
-    this.setState({submitted: true});
+
+    if (this.state.hours && this.state.minutes && this.state.rate)
+      this.setState({submitted: true});
+    else
+      Alert.alert('Fill out all fields first.');
   }
 
   // Returns to the input page
@@ -38,7 +48,15 @@ export default class App extends React.Component {
 
   render() {
     if (!this.state.submitted) {
-      return <InputPage styles={styles} onPress={this.submit}/>;
+      return (
+        <InputPage
+          styles={styles}
+          submitHours={(hours) => this.setState({hours})}
+          submitMinutes={(minutes) => this.setState({minutes})}
+          submitRate={(rate) => this.setState({rate})}
+          onPress={this.submit}
+        />
+      );
     } else {
       return <ResultPage styles={styles} onPress={this.goBack}/>;
     }
